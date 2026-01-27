bd5267d930b3b23fd3f26acfae523194e86012c6
ModiHarsh11 <162421765+ModiHarsh11@users.noreply.github.com>
2026-01-27 13:21:16 +0530

    Update designation mapping with additional roles

--- ai_formal_generator/generator/constants.py
+++ ai_formal_generator/generator/constants.py
@@ -1,24 +1,73 @@
 # Official BISAG designation mapping
 
 DESIGNATION_MAP = {
-    "Director - Administration": {
-        "hi": "निदेशक-प्रशासन",
-        "en": "Director - Administration",
-    },
     "Director General": {
         "hi": "महानिदेशक",
         "en": "Director General",
     },
-    "Project Manager": {
-        "hi": "परियोजना प्रबंधक",
-        "en": "Project Manager",
+    "Special Director General": {
+        "hi": "विशेष महानिदेशक",
+        "en": "Special Director General"
+    },
+    "Chief Vigilance Officer": {
+        "hi": "मुख्य सतर्कता अधिकारी",
+        "en": "Chief Vigilance Officer",
+    },
+    "Director-Geo Spatial Applications": {
+        "hi": "निदेशक-भू-स्थानिक अनुप्रयोग",
+        "en": "	Director-Geo Spatial Applications",
+    },
+    "Additional Director": {
+        "hi": "अपर निदेशक",
+        "en": "Additional Director",
+    },
+    "Director-Defence Applications": {
+        "hi": "निदेशक-रक्षा अनुप्रयोग",
+        "en": "Director-Defence Applications",
+    },
+    "Additional Director cum CISO": {
+        "hi": "अतिरिक्त निदेशक सह सीआईएसओ",
+        "en": "Additional Director cum CISO",
+    },
+    "Director- SATCOM Applications": {
+        "hi": "सैटकॉम अनुप्रयोगों के निदेशक",
+        "en": "Director- SATCOM Applications",
     },
     "Senior Manager": {
         "hi": "वरिष्ठ प्रबंधक",
         "en": "Senior Manager",
     },
+    "Manager - IT": {
+        "hi": "प्रबंधक - आईटी",
+        "en": "Manager - IT",
+    },
+    "Manager - Legal & Administration": {
+        "hi": "प्रबंधक - कानूनी एवं प्रशासनिक",
+        "en": "Manager - Legal & Administration",
+    },
+    "Project Manager": {
+        "hi": "परियोजना प्रबंधक",
+        "en": "Project Manager",
+    },
+    "Director -Administration": {
+        "hi": "निदेशक - प्रशासन",
+        "en": "Director -Administration",
+    },
+    "Purchase Officer": {
+        "hi": "क्रय अधिकारी",
+        "en": "Purchase Officer",
+    },
+    "Director Standardisation": {
+        "hi": "निदेशक मानकीकरण",
+        "en": "Director Standardisation",
+    },
+    "Interns": {
+        "hi": "इंटर्न्स",
+        "en": "Interns",
+    },
     "All Employee": {
         "hi": "सभी कर्मचारी",
         "en": "All Employees",
     },
+
 }
